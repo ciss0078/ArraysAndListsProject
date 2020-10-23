cbe3a73c5501ed02bad194e93a81224a66a5c03a
Abdoulahi Cisse <sentel.nv@gmail.com>
2020-10-23 16:34:05 -0700

    done arayListPro

--- ArraysAndLists/Program.cs
+++ ArraysAndLists/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ArraysAndLists
 {
@@ -9,11 +10,26 @@ namespace ArraysAndLists
             //TODO:
 
             // Create an int Array and populate numbers 1-10
+            int[] numbers = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            List<int> evens = new List<int>();
+            List<int> odds = new List<int>();
+
 
             /* Create two Lists of type int.
              * Name one List "evens"
              * Name the other List "odds"
              */
+            foreach(var num in numbers)
+            {
+                if(num % 2 == 0)
+                {
+                    evens.Add(num);
+                }
+                else
+                {
+                    odds.Add(num);
+                }
+            }
 
             /* Using either a foreach or for loop,
              * nest an if statement to check to see
@@ -21,7 +37,18 @@ namespace ArraysAndLists
              * Then add those numbers to either the evens List
              * or the odds List
              */
-
+            foreach(var even in evens)
+            {
+                Console.WriteLine(even + " is an even number");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            foreach (var odd in odds)
+            {
+                Console.WriteLine(odd + " is an odd number");
+            }
             /* Now using foeach or for loops,
              * display each List of even and odd numbers
              *
